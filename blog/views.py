dcafada855a64031da0e6655da7e6ff087852371
rems <tech1.rems@gmail.com>
2023-08-02 21:51:36 +0000

    Added views to create/edit blog post inside the site(CRUD).

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -14,3 +16,33 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     context = {'post':post}
     return render(request, 'blog/post_detail.html/', context)
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    context = {'form':form}
+    return render(request, 'blog/post_edit.html', context)
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    context = {'form':form}
+    return render(request, 'blog/post_edit.html', context)
